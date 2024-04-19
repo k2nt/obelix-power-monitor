6f12390cb269f174a352d401b2e1e90eda5b4364
khainguyen <khainguyen@umass.edu>
2024-04-19 20:31:17 +0000

    [rm] rollback db, servie

--- src/infra/di.py
+++ src/infra/di.py
@@ -2,19 +2,11 @@ from typing import List
 
 from dependency_injector import providers, containers
 
-from src import service
-
 
 class App(containers.DeclarativeContainer):
     """Application dependency injection container."""
     # infra
     config = providers.Configuration(json_files=["cfg.json"])
-    
-    # services
-    power_monitor_service = providers.Factory(
-        service.PowerMonitor,
-        data_folder=config.POWER_DATA_FOLDER
-        )
     
     
 def build(modules: List[str]):
